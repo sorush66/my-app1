515cac284cfe2038fe1bafd358747bf9deed8cf5
sorush66 <f.c.sabokpa66@gmail.com>
2026-07-16 10:37:39 +0330

    #1 lets make pizza menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,94 @@
+import { useState } from "react";
+import "./index.css"
+
+const pizzaData = [
+    {
+        name: "Focaccia",
+        ingredients: "Bread with italian olive oil and rosemary",
+        price: 6,
+        photoName: "pizzas/focaccia.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Margherita",
+        ingredients: "Tomato and mozarella",
+        price: 10,
+        photoName: "pizzas/margherita.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Spinaci",
+        ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+        price: 12,
+        photoName: "pizzas/spinaci.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Funghi",
+        ingredients: "Tomato, mozarella, mushrooms, and onion",
+        price: 12,
+        photoName: "pizzas/funghi.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Salamino",
+        ingredients: "Tomato, mozarella, and pepperoni",
+        price: 15,
+        photoName: "pizzas/salamino.jpg",
+        soldOut: true,
+    },
+    {
+        name: "Pizza Prosciutto",
+        ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+        price: 18,
+        photoName: "pizzas/prosciutto.jpg",
+        soldOut: false,
+    },
+];
+
+
 export default function App() {
-    return <h1> salam </h1>
+
+    const [pizza, setPizza] = useState(pizzaData)
+
+    return (<>
+        <Header />
+
+        <div>
+            {pizza.map((data) => (
+                <Pizza key={data.name} info={data} />
+            ))}
+        </div>
+
+        <Footer />
+    </>)
+
+}
+
+function Header() {
+    return (
+        <header className="header">
+            <h1>Fast React Pizza Co.</h1>
+        </header>
+    );
+}
+
+function Footer() {
+    return (<>
+
+    </>)
+}
+
+function Pizza({ info }) {
+    return (<>
+        <h2>{info.name}</h2>
+        <img src={info.photoName} alt={info.name} srcSet="" />
+        <p>
+            ingredients : {info.ingredients}
+        </p>
+        <div>
+            pirce : {info.price}
+        </div>
+
+    </>)
 }
